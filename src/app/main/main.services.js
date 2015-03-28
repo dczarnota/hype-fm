dd729628674e0afb511bd36517d3b6ce51233d6b
Dominic Czarnota <dczarno@gmail.com>
2015-03-27 19:15:10 -0700

    (feat) adds cache to HypedTracks factory

--- src/app/main/main.services.js
+++ src/app/main/main.services.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('hypeFm')
-  .factory('HypedTracks', function($http){
+  .factory('HypedTracks', function($http, $cacheFactory){
     var HypedTracksFactory = {
       musicInfo: {
         artists: [],
@@ -11,7 +11,7 @@ angular.module('hypeFm')
       getHypedTracks: function(){
         var url = 'http://ws.audioscrobbler.com/2.0/?method=chart.gethypedtracks&api_key=eb8e795d4ad0ecfe76dc84f885983afc&format=json';
 
-        return $http.get(url)
+        return $http.get(url, { cache: true })
           .success(function(data){
             angular.forEach(data.tracks.track, function(value){
               HypedTracksFactory.musicInfo.artists.push(value.artist.name);
