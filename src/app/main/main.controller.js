533e11cc5c40622486972e2dc584621534ee7784
Dominic Czarnota <dczarno@gmail.com>
2015-03-27 17:53:51 -0700

    (feat) gets music data for artists, track titles, and hype increase

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,10 +3,9 @@
 angular.module('hypeFm')
   .controller('MainCtrl', function($scope, HypedTracks){
 
-    HypedTracks.getHypedTracks().then(function(music){
-      $scope.artist = music.tracks.track[0].artist.name;
-      $scope.title = music.tracks.track[0].name;
-      $scope.percentageChange = music.tracks.track[0].percentagechange;
+    HypedTracks.getHypedTracks().then(function(data){
+      $scope.artist = HypedTracks.musicInfo.artists
+      $scope.title = HypedTracks.musicInfo.titles;
+      $scope.percentageChange = HypedTracks.musicInfo.percentChanges;    
     });
-
   });
--- src/app/main/main.services.js
+++ src/app/main/main.services.js
@@ -3,12 +3,21 @@
 angular.module('hypeFm')
   .factory('HypedTracks', function($http){
     var HypedTracksFactory = {
+      musicInfo: {
+        artists: [],
+        titles: [],
+        percentChanges: []
+      },
       getHypedTracks: function(){
         var url = 'http://ws.audioscrobbler.com/2.0/?method=chart.gethypedtracks&api_key=eb8e795d4ad0ecfe76dc84f885983afc&format=json';
-        
+
         return $http.get(url)
-          .then(function(response){
-            return response.data;
+          .success(function(data){
+            angular.forEach(data.tracks.track, function(value){
+              HypedTracksFactory.musicInfo.artists.push(value.artist.name);
+              HypedTracksFactory.musicInfo.titles.push(value.name);
+              HypedTracksFactory.musicInfo.percentChanges.push(value.percentagechange);
+            });
           })
           .catch(function(error){
             console.log('Error in $http get request: ' + error);
